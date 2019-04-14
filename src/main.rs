c460a9b37d2361739a42d704d4abef3a7e27bdd2
Artem <artem@webart-tech.ru>
2019-04-14 20:01:40 +0300

    начал переписывать

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,15 @@ struct Player {
 
 fn paint_screen(mut player: Player) {
 
+    let imgx = 800;
+    let imgy = 800;
+
+    let scalex = 3.0 / imgx as f32;
+    let scaley = 3.0 / imgy as f32;
+
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
     let render_map: [String; 16] =  [
         "0000000000000000".to_string(),
         "0              0".to_string(),
@@ -63,25 +72,6 @@ fn paint_screen(mut player: Player) {
         return
     }
 
-}
-
-fn main() {
-    let imgx = 800;
-    let imgy = 800;
-
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 3.0 / imgy as f32;
-
-    // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
-
-    // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.3 * x as f32) as u8;
-        let b = (0.3 * y as f32) as u8;
-        *pixel = image::Rgb([r, 0, b]);
-    }
-
     // A redundant loop to demonstrate reading image data
     for x in 0..imgx {
         for y in 0..imgy {
@@ -92,4 +82,9 @@ fn main() {
 
     // Save the image as “fractal.png”, the format is deduced from the path
     imgbuf.save("fractal.png").unwrap();
+}
+
+fn main() {
+    let mut p:Player = Player{x: 2.0, y: 2.0, a: 0.0, prev_y: 0.0, prev_x: 0.0};
+    paint_screen(p)
 }
